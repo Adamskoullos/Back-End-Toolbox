5fce6d0a5565132895cfd8b12734aa9c8ebe22ce
Adam <adamskoullos@gmail.com>
2022-01-11 17:53:01 +0000

    start altering for middleware

--- server.js
+++ server.js
@@ -4,6 +4,20 @@ const app = express();
 const path = require("path");
 const PORT = process.env.PORT || 3500;
 
+// Middleware >>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+// Built-in middleware to handle urlencoded data (form data)
+// content-type: application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: false }));
+
+// built-in middleware for json
+app.use(express.json());
+
+// serve static files
+app.use(express.static(path.join(__dirname, "/public")));
+
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
 // Home page
 app.get("^/$|index(.html)?", (req, res) => {
   // res.sendFile("./views/index.html", { root: __dirname });
