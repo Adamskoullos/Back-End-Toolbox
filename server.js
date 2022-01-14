89332d6719597afe9c549ec479239b7ef203a036
Adam <adamskoullos@gmail.com>
2022-01-14 18:24:34 +0000

    Start MVC model

--- server.js
+++ server.js
@@ -39,61 +39,20 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 // serve static files ex: /public/css/styles.css (automatically serves all files within public)
-app.use(express.static(path.join(__dirname, "/public")));
+app.use("/", express.static(path.join(__dirname, "/public"))); // Any page top level
+app.use("/subdir", express.static(path.join(__dirname, "/public"))); // any page in subdir folder
 
-// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+// Routing & Serving files >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
-// Home page
-app.get("^/$|index(.html)?", (req, res) => {
-  // res.sendFile("./views/index.html", { root: __dirname });
-  res.sendFile(path.join(__dirname, "views", "index.html"));
-});
+// Top level routes
+app.use("/", require("./routes/root"));
 
-// Basic page
-app.get("/new-page(.html)?", (req, res) => {
-  res.sendFile(path.join(__dirname, "views", "new-page.html"));
-});
+// Subdir routes
+app.use("/subdir", require("./routes/subdir"));
 
-// Redirect from old page to new page
-// Add the required status code as the first argument
-app.get("/old-page(.html)?", (req, res) => {
-  res.redirect(301, "/new-page.html"); // 302 by default
-});
+// Routing for API >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
-// Catch all
-// app.get("/*", (req, res) => {
-//   res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
-// });
-
-// Route handlers
-app.get(
-  "/hello(.html)?",
-  (req, res, next) => {
-    console.log("attempted to load hello.html");
-    next();
-  },
-  (req, res) => {
-    res.send("Hello World!");
-  }
-);
-
-// chaining route handlers
-const one = (req, res, next) => {
-  console.log("one");
-  next();
-};
-
-const two = (req, res, next) => {
-  console.log("two");
-  next();
-};
-
-const three = (req, res) => {
-  console.log("three");
-  res.send("Finished!");
-};
-
-app.get("/chain(.html)?", [one, two, three]);
+app.use("/employees", require("./routes/api/employees"));
 
 // Catch all methods and routes
 app.all("*", (req, res) => {
